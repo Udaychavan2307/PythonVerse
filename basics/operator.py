9ecdc3a5abe5c23237d70e5cfdc831a44f554f88
Uday Chavan <167866777+Udaychavan2307@users.noreply.github.com>
2026-04-28 13:28:21 +0530

    added operator.py

---
+++ basics/operator.py
@@ -0,0 +1,23 @@
+# Topic: Operators in Python
+
+a = 10
+b = 5
+
+# Arithmetic Operators
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Floor Division:", a // b)
+print("Exponent:", a ** b)
+
+# Comparison Operators
+print(a > b)
+print(a < b)
+print(a == b)
+
+# Logical Operators
+print(a > 5 and b < 10)
+print(a > 5 or b > 10)
+print(not(a > b))
