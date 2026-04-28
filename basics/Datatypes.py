112f58d7fbe6582ee5c98c27c4b12e4a3800b7f5
Uday Chavan <167866777+Udaychavan2307@users.noreply.github.com>
2026-04-28 13:16:08 +0530

    Added data_types.py with basic Python data types

---
+++ basics/Datatypes.py
@@ -0,0 +1,34 @@
+# Topic: Data Types
+# Part of my Python learning journey
+
+# Integer
+a = 10
+print(type(a))
+
+# Float
+b = 5.5
+print(type(b))
+
+# String
+c = "Hello"
+print(type(c))
+
+# Boolean
+d = True
+print(type(d))
+
+# List
+e = [1, 2, 3, 4]
+print(type(e))
+
+# Tuple
+f = (1, 2, 3)
+print(type(f))
+
+# Set
+g = {1, 2, 3}
+print(type(g))
+
+# Dictionary
+h = {"name": "Uday", "age": 22}
+print(type(h))
