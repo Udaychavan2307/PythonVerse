ca13a6719d3db8806d9ca98d6da7e4a0347daedd
Uday Chavan <167866777+Udaychavan2307@users.noreply.github.com>
2026-04-29 13:16:27 +0530

    Create if_else.py

---
+++ conditions/if_else.py
@@ -0,0 +1,13 @@
+#if_else.py
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Comparing numbers...")
+
+if a > b:
+    print("First number is greater")
+else:
+    print("Second number is greater")
+
+print("Comparison done")
