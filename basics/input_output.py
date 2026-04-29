23fc3ed3602f0f1df7265d5032b47864964036d4
Uday Chavan <167866777+Udaychavan2307@users.noreply.github.com>
2026-04-29 13:08:25 +0530

    Create input_output.py
    taking input from user and printing it

---
+++ basics/input_output.py
@@ -0,0 +1,14 @@
+# input_output.py
+
+print("----- Addition Program -----")
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b
+
+print("First number:", a)
+print("Second number:", b)
+print("Sum is:", sum)
+
+print("we are here taking input from user and then printing it")
